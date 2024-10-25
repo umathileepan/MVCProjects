dfeef6f7b35af6b3f45b6ae4ac9b84659454c158
umathileepan <thileepan_uma@yahoo.co.uk>
2024-10-25 15:44:02 +0100

    Expenses record project

---
+++ Oct24/SpendSmart/Models/SpendSmartDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace SpendSmart.Models
+{
+    public class SpendSmartDbContext : DbContext
+    {
+        public DbSet<Expense> Expenses { get; set; }
+
+        public SpendSmartDbContext(DbContextOptions<SpendSmartDbContext> options) : base(options)
+        {
+                
+        }
+    }
+}
